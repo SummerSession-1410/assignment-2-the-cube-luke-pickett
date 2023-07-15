1ed8c3368149d97d656269fa24e341f62a27b852
engooeer <engooeer@gmail.com>
2023-07-14 19:43:44 -0600

    Redid rubik's cube system with 6 sides and counterclockwise rotation

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,107 +1,362 @@
 package com.example.project;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class Cube {
 
-	static String[][][] cube = {
-			{
-					{"0r", "1r", "2r"},
-					{"3r", "4r", "5r"},
-					{"6r", "7r", "8r"}
+	static String[][] cube = {{
+					"r", "r", "r",
+					"r", "r", "r",
+					"r", "r", "r"
 			},
 			{
-					{"0b", "1b", "2b"},
-					{"3b", "4b", "5b"},
-					{"6b", "7b", "8b"}
+					"b", "b", "b",
+					"b", "b", "b",
+					"b", "b", "b"
 			},
 			{
-					{"0y", "1y", "2y"},
-					{"3y", "4y", "5y"},
-					{"6y", "7y", "8y"}
+					"o", "o", "o",
+					"o", "o", "o",
+					"o", "o", "o"
 			},
 			{
-					{"0g", "1g", "2g"},
-					{"3g", "4g", "5g"},
-					{"6g", "7g", "8g"}
+					"g", "g", "g",
+					"g", "g", "g",
+					"g", "g", "g"
 			},
 			{
-					{"0w", "1w", "2w"},
-					{"3w", "4w", "5w"},
-					{"6w", "7w", "8w"}
+					"y", "y", "y",
+					"y", "y", "y",
+					"y", "y", "y"
 			},
 			{
-					{"0o", "1o", "2o"},
-					{"3o", "4o", "5o"},
-					{"6o", "7o", "8o"}
+					"w", "w", "w",
+					"w", "w", "w",
+					"w", "w", "w"
 			}
 	};
 
+	static class EdgedFace {
+		public int current_face;
+		int[] edge1 = new int[4];
+		int[] edge2 = new int[4];
+		int[] edge3 = new int[4];
+		int[] edge4 = new int[4];
 
-	static void readFace(String[][] face)
-	{
-		System.out.println();
-		for (String[] row : face)
-		{
-			for (String value : row)
+		public EdgedFace(int face) {
+			current_face = face;
+
+			switch(face)
 			{
-				System.out.print(value);
+				case 0 -> {
+					edge1[0] = 1;
+					edge1[1] = 2;
+					edge1[2] = 5;
+					edge1[3] = 8;
+
+					edge2[0] = 4;
+					edge2[1] = 2;
+					edge2[2] = 5;
+					edge2[3] = 8;
+
+					edge3[0] = 3;
+					edge3[1] = 0;
+					edge3[2] = 3;
+					edge3[3] = 6;
+
+					edge4[0] = 5;
+					edge4[1] = 2;
+					edge4[2] = 5;
+					edge4[3] = 8;
+				}
+				case 1 -> {
+					edge1[0] = 2;
+					edge1[1] = 2;
+					edge1[2] = 5;
+					edge1[3] = 8;
+
+					edge2[0] = 4;
+					edge2[1] = 0;
+					edge2[2] = 3;
+					edge2[3] = 6;
+
+					edge3[0] = 0;
+					edge3[1] = 0;
+					edge3[2] = 3;
+					edge3[3] = 6;
+
+					edge4[0] = 5;
+					edge4[1] = 6;
+					edge4[2] = 7;
+					edge4[3] = 8;
+				}
+				case 2 -> {
+					edge1[0] = 3;
+					edge1[1] = 2;
+					edge1[2] = 5;
+					edge1[3] = 8;
+
+					edge2[0] = 4;
+					edge2[1] = 2;
+					edge2[2] = 5;
+					edge2[3] = 8;
+
+					edge3[0] = 1;
+					edge3[1] = 0;
+					edge3[2] = 3;
+					edge3[3] = 6;
+
+					edge4[0] = 5;
+					edge4[1] = 2;
+					edge4[2] = 5;
+					edge4[3] = 8;
+				}
+				case 3 -> {
+					edge1[0] = 0;
+					edge1[1] = 2;
+					edge1[2] = 5;
+					edge1[3] = 8;
+
+					edge2[0] = 2;
+					edge2[1] = 0;
+					edge2[2] = 3;
+					edge2[3] = 6;
+
+					edge3[0] = 3;
+					edge3[1] = 0;
+					edge3[2] = 3;
+					edge3[3] = 6;
+
+					edge4[0] = 5;
+					edge4[1] = 0;
+					edge4[2] = 3;
+					edge4[3] = 6;
+				}
+				case 4 -> {
+					edge1[0] = 2;
+					edge1[1] = 0;
+					edge1[2] = 1;
+					edge1[3] = 2;
+
+					edge2[0] = 3;
+					edge2[1] = 0;
+					edge2[2] = 1;
+					edge2[3] = 2;
+
+					edge3[0] = 0;
+					edge3[1] = 0;
+					edge3[2] = 1;
+					edge3[3] = 2;
+
+					edge4[0] = 1;
+					edge4[1] = 0;
+					edge4[2] = 3;
+					edge4[3] = 6;
+				}
+				case 5 -> {
+					edge1[0] = 1;
+					edge1[1] = 6;
+					edge1[2] = 7;
+					edge1[3] = 8;
+
+					edge2[0] = 0;
+					edge2[1] = 6;
+					edge2[2] = 7;
+					edge2[3] = 8;
+
+					edge3[0] = 3;
+					edge3[1] = 6;
+					edge3[2] = 7;
+					edge3[3] = 8;
+
+					edge4[0] = 2;
+					edge4[1] = 6;
+					edge4[2] = 7;
+					edge4[3] = 8;
+				}
 			}
+		}
+	}
+
+	public void turnFace(int index, String direction) {
+		EdgedFace eFace = new EdgedFace(index);
+
+		String[][] copy = new String[6][9];
+
+		for (int i=0; i<6; i++) {
+			for (int j = 0; j < 9; j++) {
+				copy[i][j] = cube[i][j];
+			}
+		}
+
+		switch (direction) {
+			// clockwise
+			case "c" -> {
+				cube[eFace.current_face][0] = copy[eFace.current_face][6];
+				cube[eFace.current_face][1] = copy[eFace.current_face][3];
+				cube[eFace.current_face][2] = copy[eFace.current_face][0];
+				cube[eFace.current_face][3] = copy[eFace.current_face][7];
+				cube[eFace.current_face][5] = copy[eFace.current_face][1];
+				cube[eFace.current_face][6] = copy[eFace.current_face][8];
+				cube[eFace.current_face][7] = copy[eFace.current_face][5];
+				cube[eFace.current_face][8] = copy[eFace.current_face][2];
+
+				// Edge 1
+				cube[eFace.edge1[0]][eFace.edge1[1]] = copy[eFace.edge4[0]][eFace.edge4[1]];
+				cube[eFace.edge1[0]][eFace.edge1[2]] = copy[eFace.edge4[0]][eFace.edge4[2]];
+				cube[eFace.edge1[0]][eFace.edge1[3]] = copy[eFace.edge4[0]][eFace.edge4[3]];
+
+				// Edge 2
+				cube[eFace.edge2[0]][eFace.edge2[1]] = copy[eFace.edge1[0]][eFace.edge1[1]];
+				cube[eFace.edge2[0]][eFace.edge2[2]] = copy[eFace.edge1[0]][eFace.edge1[2]];
+				cube[eFace.edge2[0]][eFace.edge2[3]] = copy[eFace.edge1[0]][eFace.edge1[3]];
+
+				// Edge 3
+				cube[eFace.edge3[0]][eFace.edge3[1]] = copy[eFace.edge2[0]][eFace.edge2[1]];
+				cube[eFace.edge3[0]][eFace.edge3[2]] = copy[eFace.edge2[0]][eFace.edge2[2]];
+				cube[eFace.edge3[0]][eFace.edge3[3]] = copy[eFace.edge2[0]][eFace.edge2[3]];
+
+				// Edge 4
+				cube[eFace.edge4[0]][eFace.edge4[1]] = copy[eFace.edge3[0]][eFace.edge3[1]];
+				cube[eFace.edge4[0]][eFace.edge4[2]] = copy[eFace.edge3[0]][eFace.edge3[2]];
+				cube[eFace.edge4[0]][eFace.edge4[3]] = copy[eFace.edge3[0]][eFace.edge3[3]];
+			}
+			// counter-clockwise
+			case "cc" -> {
+				cube[eFace.current_face][0] = copy[eFace.current_face][2];
+				cube[eFace.current_face][1] = copy[eFace.current_face][5];
+				cube[eFace.current_face][2] = copy[eFace.current_face][8];
+				cube[eFace.current_face][3] = copy[eFace.current_face][1];
+				cube[eFace.current_face][5] = copy[eFace.current_face][7];
+				cube[eFace.current_face][6] = copy[eFace.current_face][0];
+				cube[eFace.current_face][7] = copy[eFace.current_face][3];
+				cube[eFace.current_face][8] = copy[eFace.current_face][6];
+
+				// Edge 1
+				cube[eFace.edge1[0]][eFace.edge1[1]] = copy[eFace.edge4[0]][eFace.edge4[1]];
+				cube[eFace.edge1[0]][eFace.edge1[2]] = copy[eFace.edge4[0]][eFace.edge4[2]];
+				cube[eFace.edge1[0]][eFace.edge1[3]] = copy[eFace.edge4[0]][eFace.edge4[3]];
+
+				// Edge 2
+				cube[eFace.edge2[0]][eFace.edge2[1]] = copy[eFace.edge1[0]][eFace.edge1[1]];
+				cube[eFace.edge2[0]][eFace.edge2[2]] = copy[eFace.edge1[0]][eFace.edge1[2]];
+				cube[eFace.edge2[0]][eFace.edge2[3]] = copy[eFace.edge1[0]][eFace.edge1[3]];
+
+				// Edge 3
+				cube[eFace.edge3[0]][eFace.edge3[1]] = copy[eFace.edge2[0]][eFace.edge2[1]];
+				cube[eFace.edge3[0]][eFace.edge3[2]] = copy[eFace.edge2[0]][eFace.edge2[2]];
+				cube[eFace.edge3[0]][eFace.edge3[3]] = copy[eFace.edge2[0]][eFace.edge2[3]];
+
+				// Edge 4
+				cube[eFace.edge4[0]][eFace.edge4[1]] = copy[eFace.edge3[0]][eFace.edge3[1]];
+				cube[eFace.edge4[0]][eFace.edge4[2]] = copy[eFace.edge3[0]][eFace.edge3[2]];
+				cube[eFace.edge4[0]][eFace.edge4[3]] = copy[eFace.edge3[0]][eFace.edge3[3]];
+			}
+		}
+	}
+
+	public void showCube()
+	{
+		int i = 0;
+		for (int x = 0; x<6; x++)
+		{
+			for (int y=0; y<3; y++)
+			{
+				for (int z=0; z<3; z++)
+				{
+					System.out.print(cube[x][i++]);
+				}
+				System.out.println();
+			}
+			i = 0;
 			System.out.println();
 		}
 	}
-
-	static void rotateFace(String[][] face, boolean isClockwise)
+	public static void main(final String[] args)
+	throws IOException
 	{
-		String[][] tempFace = new String[3][3];
-		for (int i=0; i<3; i++) {
-			for (int j = 0; j < 3; j++) {
-				tempFace[i][j] = face[i][j];
-			}
-		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		Cube RubiksCube = new Cube();
 
-		// Rotates the face clockwise or counterclockwise in which faces 012345678 are rotated to 630741852
-		if (!isClockwise) {
-			for (int i=0; i<3; i++) {
-				for (int j = 0; j < 3; j++) {
-					face[i][j] = tempFace[j][i];
-				}
-			}
-			String[] tempRow = new String[3];
-			for (int k=0; k<3; k++) {
-				tempRow[k] = face[0][k];
-			}
+		ArrayList<String> userCommands = new ArrayList<String>();
 
-			face[0] = face[2];
-			face[2] = tempRow;
-		}
-		else {
-			String[] tempRow = new String[3];
-			for (int k=0; k<3; k++) {
-				tempRow[k] = face[0][k];
-			}
+		boolean argsCheck = args.length > 0;
+		int argsRunIndex = 0;
 
-			face[0] = face[2];
-			face[2] = tempRow;
+		boolean proceed = true;
 
-			for (int i=0; i<3; i++) {
-				for (int j = 0; j < 3; j++) {
-					face[i][j] = tempFace[j][i];
-				}
-			}
-			for (String[] row : face)
+		while(proceed)
+		{
+			String input;
+
+			if(!argsCheck)
 			{
-				Collections.reverse(Arrays.asList(row));
+				System.out.println("Enter a command: ");
+				input = reader.readLine();
+				if (!input.equals("s"))
+				{
+					userCommands.add(input);
+				}
+			} else {
+				if (argsRunIndex == args.length)
+				{
+					argsCheck = false;
+					input = "s";
+				} else {
+					input = args[argsRunIndex];
+					argsRunIndex++;
+				}
+			}
+
+			switch(input){
+				case "u":
+					RubiksCube.turnFace(4, "c");
+					RubiksCube.showCube();
+					break;
+				case "d":
+					break;
+				case "r":
+					RubiksCube.turnFace(0, "c");
+					RubiksCube.showCube();
+					break;
+				case "l":
+					break;
+				case "f":
+					break;
+				case "b":
+					break;
+				case "u'":
+					RubiksCube.turnFace(4, "cc");
+					RubiksCube.showCube();
+					break;
+				case "d'":
+					break;
+				case "r'":
+					RubiksCube.turnFace(0, "cc");
+					RubiksCube.showCube();
+					break;
+				case "l'":
+					break;
+				case "f'":
+					break;
+				case "b'":
+					break;
+				case "s":
+					System.out.println(userCommands.toString());
+					System.exit(0);
+					break;
+				case "q":
+					System.exit(0);
+					break;
 			}
 		}
-	}
-	public static void main(final String[] args) {
-		readFace(cube[0]);
-		rotateFace(cube[0], true);
-		readFace(cube[0]);
-		rotateFace(cube[0], false);
-		readFace(cube[0]);
+
+		RubiksCube.turnFace(0, "c");
+		RubiksCube.showCube();
 	}
 
 }
